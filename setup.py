c50e639f81734184bf4fff492a3b6065984fdcc3
wttch <wttch@wttch.com>
2024-01-18 16:03:58 +0800

    0.0.3：添加 pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='wttch-train-helper',
-    version='0.0.2',
+    version='0.0.3',
     description="Wttch's Train Helper",
     author="wttch",
     packages=[
