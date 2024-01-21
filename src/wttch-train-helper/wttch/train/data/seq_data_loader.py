0afcd1a2ba85df97e2a59934b7f19743988236d4
wttch <wttch@wttch.com>
2024-01-21 18:03:51 +0800

    0.0.9: 序列数据加载器

--- src/wttch-train-helper/wttch/train/data/seq_data_loader.py
+++ src/wttch-train-helper/wttch/train/data/seq_data_loader.py
@@ -1,9 +1,11 @@
 import random
+from typing import Union
 
 import numpy as np
 
 
-def seq_data_iter_random(corpus: np.ndarray, batch_size: int, seq_len: int) -> tuple[np.ndarray, np.ndarray]:
+def seq_data_iter_random(corpus: np.ndarray, batch_size: int, seq_len: int, label=False) \
+        -> Union[tuple[np.ndarray, np.ndarray], np.ndarray]:
     """
     随机生成序列数据，只保证序列内的顺序和原始顺序一致，不能保证小批量数据之间连续。
     Args:
@@ -15,25 +17,31 @@ def seq_data_iter_random(corpus: np.ndarray, batch_size: int, seq_len: int) -> t
 
     """
     # 偏移
-    corpus = corpus[random.randint(0, seq_len - 1):]
+    corpus = corpus[random.randint(0, seq_len):]
     # -1, 因为标签 +1, 序列的下一个数据就是标签
-    num_seqs = (len(corpus) - 1) // seq_len
+    num_seqs = (len(corpus) - (1 if label else 0)) // seq_len
     # 抽样开始的位置
     init_indices = list(range(0, num_seqs * seq_len, seq_len))
     # 打乱, batch 内序列不一定连续
     random.shuffle(init_indices)
 
-    def get_data(pos): return corpus[pos: pos + seq_len]
+    def get_data(pos):
+        return corpus[pos: pos + seq_len]
 
     # 可以生成多少批数据
     num_batches = num_seqs // batch_size
     for i in range(0, batch_size * num_batches, batch_size):
         init_indices_per_batch = init_indices[i:i + batch_size]
-        data_array = np.array([[get_data(j), get_data(j + 1)] for j in init_indices_per_batch])
-        yield data_array[:, 0], data_array[:, 1]
+        X = [get_data(j) for j in init_indices_per_batch]
+        if label:
+            Y = [get_data(j + 1) for j in init_indices_per_batch]
+            yield np.array(X), np.array(Y)
+        else:
+            yield np.array(X)
 
 
-def seq_data_iter_sequential(corpus: np.ndarray, batch_size: int, seq_len: int) -> tuple[np.ndarray, np.ndarray]:
+def seq_data_iter_sequential(corpus: np.ndarray, batch_size: int, seq_len: int, label: bool = False) \
+        -> Union[tuple[np.ndarray, np.ndarray], np.ndarray]:
     """
     顺序生成序列数据，能保证小批量数据之间连续。
     Args:
@@ -45,22 +53,27 @@ def seq_data_iter_sequential(corpus: np.ndarray, batch_size: int, seq_len: int) 
 
     """
     corpus = corpus[random.randint(0, seq_len):]
-    num_tokens = ((len(corpus) - 1) // batch_size) * batch_size
+    num_tokens = ((len(corpus) - (1 if label else 0)) // batch_size) * batch_size
 
     Xs = corpus[0: num_tokens]
-    Ys = corpus[1: num_tokens + 1]
-    Xs, Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    Xs = Xs.reshape(batch_size, -1)
+    if label:
+        Ys = corpus[1: num_tokens + 1]
+        Ys = Ys.reshape(batch_size, -1)
 
     num_batches = Xs.shape[1] // seq_len
     for i in range(0, seq_len * num_batches, seq_len):
         X = Xs[:, i: i + seq_len]
-        Y = Ys[:, i: i + seq_len]
-        yield X, Y
+        if label:
+            Y = Ys[:, i: i + seq_len]
+            yield X, Y
+        else:
+            yield X
 
 
 class SeqDataLoader:
 
-    def __init__(self, data: numpy.ndarray, batch_size: int, seq_len: int, shuffle: bool = True):
+    def __init__(self, data: np.ndarray, batch_size: int, seq_len: int, shuffle: bool = True, label: bool = False):
         """
         序列数据加载器。
         Args:
@@ -75,6 +88,7 @@ class SeqDataLoader:
         self.data = data
         self.batch_size = batch_size
         self.seq_len = seq_len
+        self.label = label
 
     def __iter__(self):
-        return self.data_iter_fn(self.data, self.batch_size, self.seq_len)
+        return self.data_iter_fn(self.data, self.batch_size, self.seq_len, self.label)
