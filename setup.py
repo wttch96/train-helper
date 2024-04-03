6d8146ca6dc1b225e71388d50b59ce47bc784388
wttch <wttch@wttch.com>
2024-04-03 11:46:41 +0800

    0.0.22: tensor_to 可以接受返回单个 tensor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='wttch-train-helper',
-    version='0.0.21',
+    version='0.0.22',
     description="Wttch's Train Helper",
     author="wttch",
     packages=[
