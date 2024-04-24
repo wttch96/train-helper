d81613e1b5bee8fd3d2e85c11d18279b7e4cea61
wttch <wttch@wttch.com>
2024-04-24 14:55:20 +0800

    0.0.30;

--- src/wttch-train-helper/wth/__init__.py
+++ src/wttch-train-helper/wth/__init__.py
@@ -7,6 +7,7 @@ class App:
     """
     为了发送异常信息。
     """
+
     def __init__(self, notification: BaseNotification, name: str = ""):
         self.notification = notification
         self.name = ""
@@ -16,7 +17,7 @@ class App:
             self._run()
         except Exception as e:
             self.notification.send_text(f"应用[{self.name}]出现异常: {str(e)}")
-            print(e)
+            raise e
 
     @abstractmethod
     def _run(self):
